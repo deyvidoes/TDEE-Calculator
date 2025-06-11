0190d5970532ab268be9428a1e3bbc951b213416
Deyvi Tineo <50240102+deyvidoes@users.noreply.github.com>
2025-06-10 20:13:38 -0400

    Check focus before hiding keyboard (#3)

--- app/src/main/java/com/deyvitineo/tdee/util/GeneralUtils.java
+++ app/src/main/java/com/deyvitineo/tdee/util/GeneralUtils.java
@@ -2,12 +2,16 @@ package com.deyvitineo.tdee.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 public class GeneralUtils {
 
     public static void hideSoftKeyboard(Activity activity){
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        View focusedView = activity.getCurrentFocus();
+        if (focusedView != null) {
+            imm.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+        }
     }
 }
